99fba02dd7702802da64aab51c60a18d12058430
kalidelhi <kalicharan777@gmail.com>
2025-01-17 09:21:12 +0530

    Create train.py

---
+++ src/train.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+import joblib
+
+# Load dataset
+data = pd.read_csv("data/dataset.csv")
+X = data.drop("target", axis=1)
+y = data["target"]
+
+# Split dataset
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Train model
+model = LogisticRegression()
+model.fit(X_train, y_train)
+
+# Save model
+joblib.dump(model, "models/model.pkl")
+print("Model trained and saved!")
